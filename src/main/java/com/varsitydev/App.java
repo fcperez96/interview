476502b263feb7b7f3ee2d68c2e1b3ff15f2effa
Picps <fcperez96@gmail.com>
2024-09-10 10:25:46 -0500

    Initial commit

---
+++ src/main/java/com/varsitydev/App.java
@@ -0,0 +1,13 @@
+package com.varsitydev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
